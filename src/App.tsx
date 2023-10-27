5a31486f8b3ade5fcd6838245249a03ff17c1b1d
Čedomir Ćebić <112137980+ccebic@users.noreply.github.com>
2023-10-27 13:57:27 +0200

    add repo

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-red-500 text-[98px] text-center">Hello World!</div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  mode: 'jit',
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+}
+
