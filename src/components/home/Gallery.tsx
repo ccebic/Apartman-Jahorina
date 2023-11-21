9ec7e35bc55b496b2877abfc1d70dc36eded5f68
Čedomir Ćebić <112137980+ccebic@users.noreply.github.com>
2023-11-21 10:15:18 +0100

    edit import for images

--- src/components/home/Gallery.tsx
+++ src/components/home/Gallery.tsx
@@ -1,9 +1,5 @@
-import ImageOne from '../../../public/images/bcg-3.webp';
+import { ImageOne, ImageTwo, ImageThree, ImageFour, ImageFive } from '../../images/modalImages.ts';
 import { Circle, GalleryItem } from '../../layout/components';
-import ImageTwo from '../../images/vilaMarina/marina7-min.webp';
-import ImageFour from '../../images/vilaJelena/jelena6-min.webp';
-import ImageFive from '../../images/vilaMarina/marina6-min.webp';
-import ImageThree from '../../images/vilaMarina/marina13-min.webp';
 
 export const Gallery = () => {
    return (
---
+++ src/images/modalImages.ts
@@ -0,0 +1,8 @@
+import ImageOne from '../../public/images/bcg-3.webp';
+import ImageTwo from '../images/vilaMarina/marina7-min.webp';
+import ImageFour from '../images/vilaJelena/jelena6-min.webp';
+import ImageFive from '../images/vilaMarina/marina6-min.webp';
+import ImageThree from '../images/vilaMarina/marina13-min.webp';
+
+
+export  { ImageOne, ImageTwo, ImageFour, ImageFive, ImageThree}
